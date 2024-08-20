102b848eeb09758922379ecd83a86c69c7f5a6f9
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-08-20 07:08:49 +0200

    chore: remove unused part of nvim config

--- macos/.config/nvim/lua/plugins/ui.lua
+++ macos/.config/nvim/lua/plugins/ui.lua
@@ -126,8 +126,6 @@ return {
 					enabled = false,
 				},
 				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
 					["cmp.entry.get_documentation"] = true,
 				},
 			},
