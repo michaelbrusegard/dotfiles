0bdf969e79040d0621da514554e0fc04be258a8e
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-09-12 12:42:42 +0200

    fix: image support for nvim jupyter notebooks

--- macos/.config/nvim/lua/plugins/util.lua
+++ macos/.config/nvim/lua/plugins/util.lua
@@ -1,6 +1,35 @@
 return {
   -- Library used by other plugins
   { 'nvim-lua/plenary.nvim', lazy = true },
+  -- Image support
+  {
+    '3rd/image.nvim',
+    dependencies = { 'leafo/magick' },
+    event = 'LazyFile',
+    opts = {
+      backend = 'kitty',
+      integrations = {
+        markdown = {
+          enabled = false,
+          clear_in_insert_mode = true,
+          download_remote_images = true,
+          only_render_image_at_cursor = true,
+          filetypes = { 'markdown', 'quarto' },
+        },
+        html = {
+          enabled = false,
+        },
+        css = {
+          enabled = false,
+        },
+      },
+      max_height_window_percentage = 100,
+      max_width_window_percentage = 100,
+      window_overlap_clear_enabled = true,
+      editor_only_render_when_focused = false,
+      -- hijack_file_patterns = { '*.png', '*.jpg', '*.jpeg', '*.gif', '*.webp', '*.avif' },
+    },
+  },
   -- Learn the best way to do vim commands
   {
     'm4xshen/hardtime.nvim',
