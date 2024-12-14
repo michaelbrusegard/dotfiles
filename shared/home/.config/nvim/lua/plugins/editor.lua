0d0397a5c062526d9512b15b9fabc03140d34bd2
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-12-14 04:30:13 +0100

    feat: add yazi file manager

--- shared/home/.config/nvim/lua/plugins/editor.lua
+++ shared/home/.config/nvim/lua/plugins/editor.lua
@@ -49,7 +49,18 @@ return {
   },
   {
     'stevearc/oil.nvim',
-    opts = {},
+    opts = {
+      default_file_explorer = true,
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+        natural_order = true,
+        is_always_hidden = function(name, _)
+          return name == '..' or name == '.git' or name == '.DS_Store'
+        end,
+      },
+    },
     keys = {
       {
         '-',
