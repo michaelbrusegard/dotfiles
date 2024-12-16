ee4bf8b4f6e216a2b578977ffc703100ebe80ce6
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-12-16 09:40:06 +0100

    fix: move disabled plugins to their respective files

--- shared/home/.config/nvim/lua/plugins/core.lua
+++ shared/home/.config/nvim/lua/plugins/core.lua
@@ -7,25 +7,4 @@ return {
       colorscheme = 'catppuccin',
     },
   },
-  {
-    'folke/snacks.nvim',
-    -- Use latest when underline bug is fixed
-    version = '2.9.0',
-    opts = {
-      indent = {
-        indent = {
-          char = '▏',
-        },
-        scope = {
-          underline = true,
-          char = '▏',
-        },
-      },
-    },
-  },
-  { 'folke/tokyonight.nvim', enabled = false },
-  { 'folke/trouble.nvim', enabled = false },
-  { 'echasnovski/mini.pairs', enabled = false },
-  { 'nvim-neo-tree/neo-tree.nvim', enabled = false },
-  { 'akinsho/bufferline.nvim', enabled = false },
 }
