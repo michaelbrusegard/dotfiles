b98d2d590189a7b466185b5f9bcea5f4f075ea32
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-12-16 09:32:50 +0100

    chore: fix keymaps

--- shared/home/.config/nvim/lua/config/keymaps.lua
+++ shared/home/.config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,6 @@
 -- Copy to system clipboard with super + c
-vim.keymap.set({ 'n', 'x' }, '<char-0xca>', '"+y', { silent = true })
+vim.keymap.set('n', '<char-0xca>', '"+yy', { silent = true })
+vim.keymap.set('x', '<char-0xca>', '"+y', { silent = true })
 
 -- Paste from system clipboard with super + v
 vim.keymap.set({ 'n', 'x' }, '<char-0xcb>', '"+p', { silent = true })
@@ -7,9 +8,6 @@ vim.keymap.set('i', '<char-0xcb>', function()
   return vim.api.nvim_replace_termcodes('<esc>"+pi', true, true, true)
 end, { expr = true, silent = true })
 vim.keymap.set('c', '<char-0xcb>', '<c-R>+', { silent = true })
-
--- Cut to system clipboard with super + x
-vim.keymap.set({ 'x', 'n' }, '<char-0xcc>', '"+x', { silent = true })
 
 -- Save file with super + s
 vim.keymap.set({ 'i', 'x', 'n', 's' }, '<char-0xcd>', '<cmd>w<cr><esc>', { silent = true })
