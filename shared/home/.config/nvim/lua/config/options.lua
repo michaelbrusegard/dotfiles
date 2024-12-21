4b72e276a1ae8dc70404ae2145a0bafdcdf09ee2
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-12-21 08:46:33 +0100

    fix: support other javascript formatters

--- shared/home/.config/nvim/lua/config/options.lua
+++ shared/home/.config/nvim/lua/config/options.lua
@@ -6,3 +6,6 @@ local venv = vim.fn.expand('~/.pyenv/versions/neovim')
 vim.g.python3_host_prog = venv .. '/bin/python'
 vim.env.PATH = venv .. '/bin:' .. vim.env.PATH
 vim.env.PYENV_VERSION = vim.fn.system('pyenv version'):match('(%S+)%s+%(.-%)')
+
+-- Require config for prettier
+vim.g.lazyvim_prettier_needs_config = true
