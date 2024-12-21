9336f839dc86689f33e836c8c18d345460763817
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-12-21 10:47:48 +0100

    feat: add ipsec vpn server to espresso for remote netflix

--- espresso/scripts/login.sh
+++ espresso/scripts/login.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-set -x  # Print commands and their arguments as they are executed
+set -x # Print commands and their arguments as they are executed
+
+# Enable Firewall
+sudo systemctl enable ufw
 
 # Enable SSH
 sudo systemctl enable ssh
 
 # Enable wake on lan
 sudo systemctl enable wol.service
+
+# Enable Docker
+sudo systemctl enable docker
