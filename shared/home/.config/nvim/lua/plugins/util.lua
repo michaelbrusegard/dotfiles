03d7edeb511d4771ee59e432603f7ef58c196ccb
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-12-26 05:29:34 +0100

    feat: add screenshot support to nvim

--- shared/home/.config/nvim/lua/plugins/util.lua
+++ shared/home/.config/nvim/lua/plugins/util.lua
@@ -16,4 +16,30 @@ return {
       },
     },
   },
+  {
+    'michaelrommel/nvim-silicon',
+    lazy = true,
+    cmd = 'Silicon',
+    main = 'nvim-silicon',
+    keys = {
+      {
+        '<leader>fs',
+        mode = { 'n', 'x' },
+        "<cmd>lua require('nvim-silicon').clip()<cr>",
+        desc = 'Take Screenshot',
+      },
+    },
+    opts = {
+      disable_defaults = true,
+      language = function()
+        return vim.bo.filetype
+      end,
+      window_title = function()
+        return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ':t')
+      end,
+      to_clipboard = true,
+      gobble = true,
+      num_separator = ' ',
+    },
+  },
 }
