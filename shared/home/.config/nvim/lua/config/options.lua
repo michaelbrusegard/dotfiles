68deb6a7f3e2fddd7ba19dd33436ac48c940fbea
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-01-18 12:34:26 +0100

    feat: set snacks as default picker

--- shared/home/.config/nvim/lua/config/options.lua
+++ shared/home/.config/nvim/lua/config/options.lua
@@ -7,5 +7,8 @@ vim.g.python3_host_prog = venv .. '/bin/python'
 vim.env.PATH = venv .. '/bin:' .. vim.env.PATH
 vim.env.PYENV_VERSION = vim.fn.system('pyenv version'):match('(%S+)%s+%(.-%)')
 
+-- Set snacks as the picker
+vim.g.lazyvim_picker = 'snacks'
+
 -- Require config for prettier
 vim.g.lazyvim_prettier_needs_config = true
