85ed8d0aca848317d3d49d6f08b2034266ae14b9
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-01-21 09:33:07 +0100

    feat: fix pasting and add codecompanion to nvim

--- shared/home/.config/nvim/lua/plugins/ai.lua
+++ shared/home/.config/nvim/lua/plugins/ai.lua
@@ -1,27 +1,68 @@
 return {
   { import = 'lazyvim.plugins.extras.ai.copilot' },
-  { import = 'lazyvim.plugins.extras.ai.copilot-chat' },
   { import = 'lazyvim.plugins.extras.ai.supermaven' },
   {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    opts = {
-      model = 'claude-3.5-sonnet',
-      show_help = false,
-      auto_insert_mode = false,
-      separator = '───',
+    'olimorris/codecompanion.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
     },
-    config = function(_, opts)
-      local chat = require('CopilotChat')
-      vim.api.nvim_create_autocmd('BufEnter', {
-        pattern = 'copilot-chat',
-        callback = function()
-          vim.opt_local.relativenumber = false
-          vim.opt_local.number = false
-          vim.keymap.set('n', '<C-s>', '<cmd>CopilotChatStop<cr>', { buffer = true })
+    keys = {
+      { '<leader>a', '', desc = '+ai', mode = { 'n', 'v' } },
+      {
+        '<leader>aa',
+        function()
+          return require('codecompanion').toggle()
         end,
-      })
-
-      chat.setup(opts)
-    end,
+        desc = 'Toggle Chat',
+        mode = { 'n', 'v' },
+      },
+      {
+        '<leader>aq',
+        function()
+          local input = vim.fn.input('Quick Chat: ')
+          if input ~= '' then
+            local mode = vim.fn.mode()
+            if mode == 'v' or mode == 'V' then
+              local start_pos = vim.fn.getpos("'<")
+              local end_pos = vim.fn.getpos("'>")
+              local lines = vim.api.nvim_buf_get_text(0, start_pos[2] - 1, 0, end_pos[2] - 1, -1, {})
+              local selected_text = table.concat(lines, '\n')
+              vim.cmd('CodeCompanion ' .. selected_text .. ' ' .. input)
+            else
+              vim.cmd('CodeCompanion ' .. input)
+            end
+          end
+        end,
+        desc = 'Quick Chat',
+        mode = { 'n', 'v' },
+      },
+      {
+        '<leader>ap',
+        '<cmd>CodeCompanionActions<cr>',
+        desc = 'Prompt Actions',
+        mode = { 'n', 'v' },
+      },
+    },
+    opts = {
+      strategies = {
+        chat = {
+          adapter = 'copilot_claude',
+        },
+      },
+      adapters = {
+        copilot_claude = function()
+          return require('codecompanion.adapters').extend(
+            'copilot',
+            { schema = { model = { default = 'claude-3.5-sonnet' } } }
+          )
+        end,
+      },
+      display = {
+        chat = {
+          intro_message = '',
+        },
+      },
+    },
   },
 }
