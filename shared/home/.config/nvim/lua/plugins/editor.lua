f6240f007f362c31343dabe39b50ad271e9c0f05
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-01-26 16:06:24 +0100

    feat: add overseer to nvim

--- shared/home/.config/nvim/lua/plugins/editor.lua
+++ shared/home/.config/nvim/lua/plugins/editor.lua
@@ -3,6 +3,7 @@ return {
   { import = 'lazyvim.plugins.extras.editor.dial' },
   { import = 'lazyvim.plugins.extras.editor.harpoon2' },
   { import = 'lazyvim.plugins.extras.editor.mini-move' },
+  { import = 'lazyvim.plugins.extras.editor.overseer' },
   {
     'mrjones2014/smart-splits.nvim',
     opts = {},
