ab5f90a800f40815b232f54ff05d177b7ad9b443
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-02-16 16:05:22 +0100

    feat: setup qmk and gstreamer

--- shared/home/.config/nvim/lua/plugins/lang.lua
+++ shared/home/.config/nvim/lua/plugins/lang.lua
@@ -1,6 +1,5 @@
 return {
   { import = 'lazyvim.plugins.extras.lang.angular' },
-  { import = 'lazyvim.plugins.extras.lang.clangd' },
   { import = 'lazyvim.plugins.extras.lang.cmake' },
   { import = 'lazyvim.plugins.extras.lang.docker' },
   { import = 'lazyvim.plugins.extras.lang.git' },
