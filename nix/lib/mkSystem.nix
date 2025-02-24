b8b7cbbb761a23b9e8a4f84f10991cd01e57347a
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-02-24 10:18:10 +0100

    feat: add home manager module and structure config

--- nix/lib/mkSystem.nix
+++ nix/lib/mkSystem.nix
@@ -1,22 +1,35 @@
 inputs:
 { system, user, name }:
 let
-  configPath = ./hosts/${name}/configuration.nix;
+  configPath = import ./hosts/${name};
+  userPath = import ./users/${user};
+
+  homeManagerConfig = {
+    home-manager = {
+      useUserPackages = true;
+      useGlobalPkgs = true;
+      users.${user} = userPath;
+    };
+  };
+
+  commonArgs = {
+    inherit system;
+    specialArgs = { inherit inputs name; };
+    modules = [
+      configPath
+      homeManagerConfig
+    ];
+  };
+
 in
 if builtins.match ".*-darwin" system != null
-then inputs.darwin.lib.darwinSystem {
-  inherit system;
-  specialArgs = { inherit inputs name; };
-  modules = [
-    user
-    configPath
+then inputs.darwin.lib.darwinSystem (commonArgs // {
+  modules = commonArgs.modules ++ [
+    inputs.home-manager.darwinModules.home-manager
   ];
-}
-else inputs.nixpkgs.lib.nixosSystem {
-  inherit system;
-  specialArgs = { inherit inputs name; };
-  modules = [
-    user
-    configPath
+})
+else inputs.nixpkgs.lib.nixosSystem (commonArgs // {
+  modules = commonArgs.modules ++ [
+    inputs.home-manager.nixosModules.home-manager
   ];
-}
+})
