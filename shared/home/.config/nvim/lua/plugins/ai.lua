fe956627caa7a6e7f3d68b915391d4166199584a
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-02-28 05:04:43 +0100

    feat: witch to claude 3.7 sonet for nvim

--- shared/home/.config/nvim/lua/plugins/ai.lua
+++ shared/home/.config/nvim/lua/plugins/ai.lua
@@ -5,7 +5,7 @@ return {
   {
     'CopilotC-Nvim/CopilotChat.nvim',
     opts = {
-      model = 'claude-3.5-sonnet',
+      model = 'claude-3.7-sonnet',
       show_help = false,
       auto_insert_mode = false,
       separator = '───',
