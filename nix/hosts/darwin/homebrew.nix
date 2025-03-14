f71ca93218e44c51505f28a8ed9ba52077020da5
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-14 04:49:02 +0100

    feat: add homebrew config for darwin

---
+++ nix/hosts/darwin/homebrew.nix
@@ -0,0 +1,19 @@
+{ ... }: {
+  homebrew = {
+    enable = true;
+    masApps = {
+      "Proton Pass for Safari" = 6502835663;
+      "Wipr" = 1662217862;
+      "WireGuard" = 1451685025;
+      "Messenger" = 1480068668;
+      "Affinity Designer 2" = 1616831348;
+      "Affinity Photo 2" = 1616822987;
+      "Affinity Publisher 2" = 1606941598;
+      "Keynote" = 409183694;
+      "Numbers" = 409203825;
+      "Pages" = 409201541;
+      "Developer" = 640199958;
+      "Xcode" = 497799835;
+    };
+  };
+};
