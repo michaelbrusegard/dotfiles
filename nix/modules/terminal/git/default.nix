b771432dc888e703fa7e68304e8546478c790d7f
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-15 04:45:40 +0100

    feat: convert modules to actually be nix modules

--- nix/modules/terminal/git/default.nix
+++ nix/modules/terminal/git/default.nix
@@ -1,59 +1,69 @@
-{ pkgs, ... }: {
-  programs.git = {
-    enable = true;
-    userName = "Michael Brusegard";
-    userEmail = "56915010+michaelbrusegard@users.noreply.github.com";
-    extraConfig = {
-      init = {
-        defaultBranch = "main";
-      };
-      push = {
-        autoSetupRemote = true;
-      };
-      pull = {
-        rebase = false;
-      };
-      status = {
-        showUntrackedFiles = "all";
-        showStash = true;
-      };
-      rebase = {
-        autoStash = true;
-      };
-      core = {
-        editor = "nvim";
-        pager = "delta";
-      };
-      commit = {
-        gpgSign = true;
-        signingKey = "~/.ssh/github_ed25519";
-      };
-      gpg = {
-        format = "ssh";
-      };
-      merge = {
-        conflictstyle = "diff3";
-      };
-      diff = {
-        colorMoved = "default";
-      };
-      interactive = {
-        diffFilter = "delta --color-only";
-      };
-    };
-    delta = {
-      enable = true;
-      options = {
-        navigate = true;
-        side-by-side = true;
-      };
-    };
-    ignores = [
-      ".DS_Store"
-      "*.swp"
-      "*.swo"
-      ".env"
-      "node_modules"
-    ];
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.modules.terminal.git;
+in {
+  options.modules.terminal.git = {
+    enable = lib.mkEnableOption "git configuration";
   };
-};
+
+  config = lib.mkIf cfg.enable {
+    programs.git = {
+      enable = true;
+      userName = "Michael Brusegard";
+      userEmail = "56915010+michaelbrusegard@users.noreply.github.com";
+      extraConfig = {
+        init = {
+          defaultBranch = "main";
+        };
+        push = {
+          autoSetupRemote = true;
+        };
+        pull = {
+          rebase = false;
+        };
+        status = {
+          showUntrackedFiles = "all";
+          showStash = true;
+        };
+        rebase = {
+          autoStash = true;
+        };
+        core = {
+          editor = "nvim";
+          pager = "delta";
+        };
+        commit = {
+          gpgSign = true;
+          signingKey = "~/.ssh/github_ed25519";
+        };
+        gpg = {
+          format = "ssh";
+        };
+        merge = {
+          conflictstyle = "diff3";
+        };
+        diff = {
+          colorMoved = "default";
+        };
+        interactive = {
+          diffFilter = "delta --color-only";
+        };
+      };
+      delta = {
+        enable = true;
+        options = {
+          navigate = true;
+          side-by-side = true;
+        };
+      };
+      ignores = [
+        ".DS_Store"
+        "*.swp"
+        "*.swo"
+        ".env"
+        "node_modules"
+      ];
+    };
+  };
+}
--- nix/users/michaelbrusegard/default.nix
+++ nix/users/michaelbrusegard/default.nix
@@ -1,13 +1,18 @@
 { ... }: {
   home.stateVersion = "25.05";
-  catppuccin.flavor = "mocha";
-  catppuccin.enable = true;
-  imports = [
-    ../../programs/git
-    ../../programs/zsh
-    ../../programs/yazi
-    ../../programs/neovim
-    ../../programs/wezterm
-    ../../programs/zen-browser
-  ];
+  catppuccin = {
+    enable = true;
+    flavor = "mocha";
+  };
+  modules = {
+    programs = {
+      zenBrowser.enable = true;
+    };
+    terminal = {
+      git.enable = true;
+      neovim.enable = true;
+      shell.enable = true;
+      wezterm.enable = true;
+    };
+  };
 };
