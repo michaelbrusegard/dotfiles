4f902ec956717a2eeb29c9c2c903bbf19d4e9b02
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-16 11:58:35 +0100

    feat: more yazi configuration

--- nix/modules/terminal/yazi/default.nix
+++ nix/modules/terminal/yazi/default.nix
@@ -10,6 +10,32 @@ in {
   config = lib.mkIf cfg.enable {
     programs.yazi = {
       enable = true;
+      enableZshIntegration = true;
+      shellWrapperName = "y";
+      settings = {
+        manager = {
+          sort_by = "alphabetical";
+          sort_sensitive = true;
+          sort_dir_first = true;
+          linemode = "mtime";
+          show_hidden = true;
+          show_symlink = true;
+          scrolloff = 8;
+          mouse_events = [
+            "click"
+            "scroll"
+            "touch"
+            "move"
+            "drag"
+          ];
+          title_format = "{cwd}";
+        };
+      };
+      keymap = {
+        input.prepend_keymap = [
+          { on = "<Esc>"; run = "close";  desc = "Cancel input" }
+        ];
+      };
     };
     catppuccin.yazi = {
       enable = true;
