eb59a5f1bba3d0e79fd38c3485ca9f31e1b7262b
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-18 00:47:36 +0100

    chore: empty secrets.nix file to make the flake runnable

---
+++ nix/secrets.nix
@@ -0,0 +1,48 @@
+{
+  ssh = {
+    desktop = {
+      hostName = "";
+      port = "";
+    };
+    espresso = {
+      hostName = "";
+      port = "";
+    };
+    dingseboms = {
+      hostName = "";
+      port = "";
+    };
+    duppeditt = {
+      hostName = "";
+      port = "";
+    };
+    gluteus = {
+      hostName = "";
+      port = "";
+    };
+    noodlebar = {
+      hostName = "";
+      port = "";
+    };
+    phoenix = {
+      hostName = "";
+      port = "";
+    };
+    meieri = {
+      hostName = "";
+      port = "";
+    };
+  };
+  wol = {
+    desktop = {
+      ip = "";
+      mac = "";
+      port = "";
+    };
+    espresso = {
+      ip = "";
+      mac = "";
+      port = "";
+    };
+  };
+}
