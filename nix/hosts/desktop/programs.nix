25fd21c979ac4e12278b39c5474c3028dc37c687
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-19 07:28:56 +0100

    feat: add basic waybar config

--- nix/hosts/desktop/programs.nix
+++ nix/hosts/desktop/programs.nix
@@ -84,6 +84,26 @@
     };
     waybar = {
       enable = true;
+      systemd.enable = true;
+      settings = {
+        mainBar = {
+          layer = "top";
+          position = "top";
+          height = 24;
+          modules-left = [ "hyprland/window" ];
+          modules-right = [ "hyprland/workspaces" "bluetooth" "network" "clock" ];
+          "hyprland/window" = {
+            separate-outputs = true;
+          };
+          "hyprland/workspaces" = {
+            active-only = true;
+          };
+          "clock" = {
+            format = "{:%d %b %H:%M}";
+          };
+        };
+      };
+      style = {};
     };
     catppuccin.waybar = {
       enable = true;
