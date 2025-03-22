167b329f0d40acd63b68f35616fc46a591403f8e
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-22 22:48:12 +0100

    fix: dns configuration to work for both darwin and linux

--- utils/config/darwin.nix
+++ utils/config/darwin.nix
@@ -13,6 +13,12 @@
   networking = {
     computerName = hostname;
     localHostName = hostname;
+    dns = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "2606:4700:4700::1111"
+      "2606:4700:4700::1001"
+    ];
   };
   users.users.${username} = {
     home = "/Users/${username}";
