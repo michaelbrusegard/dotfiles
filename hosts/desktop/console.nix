f4da07c063fb4a2683789957b496c41b35895884
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-23 01:11:20 +0100

    fix: colors to not include #

--- hosts/desktop/console.nix
+++ hosts/desktop/console.nix
@@ -4,22 +4,22 @@
     font = "SF Mono";
     keyMap = "us";
     colors = [
-      "${colors.mocha.base}"
-      "${colors.mocha.red}"
-      "${colors.mocha.green}"
-      "${colors.mocha.yellow}"
-      "${colors.mocha.blue}"
-      "${colors.mocha.mauve}"
-      "${colors.mocha.teal}"
-      "${colors.mocha.text}"
-      "${colors.mocha.surface0}"
-      "${colors.mocha.peach}"
-      "${colors.mocha.sapphire}"
-      "${colors.mocha.rosewater}"
-      "${colors.mocha.lavender}"
-      "${colors.mocha.pink}"
-      "${colors.mocha.sky}"
-      "${colors.mocha.crust}"
+      "${builtins.substring 1 6 colors.mocha.base}"
+      "${builtins.substring 1 6 colors.mocha.red}"
+      "${builtins.substring 1 6 colors.mocha.green}"
+      "${builtins.substring 1 6 colors.mocha.yellow}"
+      "${builtins.substring 1 6 colors.mocha.blue}"
+      "${builtins.substring 1 6 colors.mocha.mauve}"
+      "${builtins.substring 1 6 colors.mocha.teal}"
+      "${builtins.substring 1 6 colors.mocha.text}"
+      "${builtins.substring 1 6 colors.mocha.surface0}"
+      "${builtins.substring 1 6 colors.mocha.peach}"
+      "${builtins.substring 1 6 colors.mocha.sapphire}"
+      "${builtins.substring 1 6 colors.mocha.rosewater}"
+      "${builtins.substring 1 6 colors.mocha.lavender}"
+      "${builtins.substring 1 6 colors.mocha.pink}"
+      "${builtins.substring 1 6 colors.mocha.sky}"
+      "${builtins.substring 1 6 colors.mocha.crust}"
     ];
   };
 }
