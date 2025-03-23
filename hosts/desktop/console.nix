64e19abe750b39102e5db8d1c54174c17958ca14
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-23 03:06:32 +0100

    fix: console colors

--- hosts/desktop/console.nix
+++ hosts/desktop/console.nix
@@ -4,14 +4,14 @@
     font = "SF Mono";
     keyMap = "us";
     colors = [
-      "${builtins.substring 1 6 colors.mocha.base}"
+      "${builtins.substring 1 6 colors.mocha.crust}"
       "${builtins.substring 1 6 colors.mocha.red}"
       "${builtins.substring 1 6 colors.mocha.green}"
       "${builtins.substring 1 6 colors.mocha.yellow}"
       "${builtins.substring 1 6 colors.mocha.blue}"
       "${builtins.substring 1 6 colors.mocha.mauve}"
       "${builtins.substring 1 6 colors.mocha.teal}"
-      "${builtins.substring 1 6 colors.mocha.text}"
+      "${builtins.substring 1 6 colors.mocha.subtext0}"
       "${builtins.substring 1 6 colors.mocha.surface0}"
       "${builtins.substring 1 6 colors.mocha.peach}"
       "${builtins.substring 1 6 colors.mocha.sapphire}"
@@ -19,7 +19,7 @@
       "${builtins.substring 1 6 colors.mocha.lavender}"
       "${builtins.substring 1 6 colors.mocha.pink}"
       "${builtins.substring 1 6 colors.mocha.sky}"
-      "${builtins.substring 1 6 colors.mocha.crust}"
+      "${builtins.substring 1 6 colors.mocha.text}"
     ];
   };
 }
