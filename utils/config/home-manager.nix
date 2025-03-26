4989ad459d147d89dc52690f79aa304de7f9c420
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-26 12:46:34 +0100

    fix: set home-manager backup extension

--- utils/config/home-manager.nix
+++ utils/config/home-manager.nix
@@ -3,6 +3,7 @@
   home-manager = {
     useUserPackages = true;
     useGlobalPkgs = true;
+    backupFileExtension = "backup";
     users.${username} = {
       imports = [
         catppuccin.homeManagerModules.catppuccin
