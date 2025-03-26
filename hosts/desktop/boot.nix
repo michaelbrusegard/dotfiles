f273b119b5a144685d4f586ce9ce2820a9945318
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-26 22:56:15 +0100

    fix: add missing luks device to configuration

--- hosts/desktop/boot.nix
+++ hosts/desktop/boot.nix
@@ -7,7 +7,10 @@
     };
     initrd = {
       availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
-      luks.devices."luks-3326ca76-4cad-48a9-9c88-6aadd59e63fa".device = "/dev/disk/by-uuid/3326ca76-4cad-48a9-9c88-6aadd59e63fa";
+      luks.devices = {
+        "luks-c3fbdb7e-8668-43d3-ae74-1c8dd400f0d8".device = "/dev/disk/by-uuid/c3fbdb7e-8668-43d3-ae74-1c8dd400f0d8"
+        "luks-3326ca76-4cad-48a9-9c88-6aadd59e63fa".device = "/dev/disk/by-uuid/3326ca76-4cad-48a9-9c88-6aadd59e63fa"
+      };
     };
   };
 }
