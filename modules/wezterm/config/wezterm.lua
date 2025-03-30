a251ebf9030a2050fd0f11c0aa9a2bb935699850
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-30 04:23:07 +0200

    feat: fix wezterm hyprland tab bar issue

--- modules/wezterm/config/wezterm.lua
+++ modules/wezterm/config/wezterm.lua
@@ -7,22 +7,22 @@ package.path = package.path .. ';' .. wezterm.config_dir .. '/lua/?.lua'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- -- Basic options and theme
--- require('config.options')(config)
---
--- -- Keybinds
--- require('config.keybinds')(config)
---
--- -- Manage plugins
--- local plugins_dir = wezterm.config_dir .. '/lua/plugins'
--- local files = io.popen('ls ' .. wezterm.config_dir .. '/lua/plugins/*.lua')
---
--- if files then
---   for file in files:lines() do
---     local module_name = file:gsub(plugins_dir .. '/', ''):gsub('%.lua$', '')
---     require('plugins.' .. module_name)(config)
---   end
--- end
+-- Basic options and theme
+require('config.options')(config)
+
+-- Keybinds
+require('config.keybinds')(config)
+
+-- Manage plugins
+local plugins_dir = wezterm.config_dir .. '/lua/plugins'
+local files = io.popen('ls ' .. wezterm.config_dir .. '/lua/plugins/*.lua')
+
+if files then
+  for file in files:lines() do
+    local module_name = file:gsub(plugins_dir .. '/', ''):gsub('%.lua$', '')
+    require('plugins.' .. module_name)(config)
+  end
+end
 
 -- Finally, return the configuration to wezterm
 return config
