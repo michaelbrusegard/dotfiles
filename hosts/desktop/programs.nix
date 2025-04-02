b0ab07092d6dc3c545f24380cd6b255382a350c3
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-02 17:40:55 +0200

    chore: minimal regreet

--- hosts/desktop/programs.nix
+++ hosts/desktop/programs.nix
@@ -3,36 +3,36 @@
     dconf.enable = true;
     regreet = {
       enable = true;
-      cursorTheme = {
-        name = "macOS";
-        package = pkgs.apple-cursor;
-      };
-      font = {
-        name = "SF Pro";
-      };
-      theme = {
-        name = "Adwaita";
-      };
-      iconTheme = {
-        name = "Adwaita";
-      };
-      settings = {
-        background = {
-          path = ../../assets/wallpapers/twilight-peaks.png;
-          fit = "Cover";
-        };
-        appearance = {
-          greeting_msg = "Welcome";
-        };
-        "widget.clock" = {
-          format = "%A %e %b %H:%M";
-          timezone = "Europe/Oslo";
-          label_width = 200;
-        };
-        GTK = {
-          application_prefer_dark_theme = true;
-        };
-      };
+      # cursorTheme = {
+      #   name = "macOS";
+      #   package = pkgs.apple-cursor;
+      # };
+      # font = {
+      #   name = "SF Pro";
+      # };
+      # theme = {
+      #   name = "Adwaita";
+      # };
+      # iconTheme = {
+      #   name = "Adwaita";
+      # };
+      # settings = {
+      #   background = {
+      #     path = ../../assets/wallpapers/twilight-peaks.png;
+      #     fit = "Cover";
+      #   };
+      #   appearance = {
+      #     greeting_msg = "Welcome";
+      #   };
+      #   "widget.clock" = {
+      #     format = "%A %e %b %H:%M";
+      #     timezone = "Europe/Oslo";
+      #     label_width = 200;
+      #   };
+      #   GTK = {
+      #     application_prefer_dark_theme = true;
+      #   };
+      # };
     };
   };
 }
