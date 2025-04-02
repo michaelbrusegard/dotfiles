daa2d57a60975d994c100fdac7c28fa2aa2916a0
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-02 18:25:44 +0200

    feat: add logind

--- hosts/desktop/programs.nix
+++ hosts/desktop/programs.nix
@@ -3,36 +3,38 @@
     dconf.enable = true;
     regreet = {
       enable = true;
-      # cursorTheme = {
-      #   name = "macOS";
-      #   package = pkgs.apple-cursor;
-      # };
-      # font = {
-      #   name = "SF Pro";
-      # };
-      # theme = {
-      #   name = "Adwaita";
-      # };
-      # iconTheme = {
-      #   name = "Adwaita";
-      # };
-      # settings = {
-      #   background = {
-      #     path = ../../assets/wallpapers/twilight-peaks.png;
-      #     fit = "Cover";
-      #   };
-      #   appearance = {
-      #     greeting_msg = "Welcome";
-      #   };
-      #   "widget.clock" = {
-      #     format = "%A %e %b %H:%M";
-      #     timezone = "Europe/Oslo";
-      #     label_width = 200;
-      #   };
-      #   GTK = {
-      #     application_prefer_dark_theme = true;
-      #   };
-      # };
+      cursorTheme = {
+        name = "macOS";
+        package = pkgs.apple-cursor;
+      };
+      font = {
+        name = "SF Pro";
+      };
+      theme = {
+        name = "Adwaita";
+        package = pkgs.gnome-themes-extra;
+      };
+      iconTheme = {
+        name = "Adwaita";
+        package = pkgs.adwaita-icon-theme;
+      };
+      settings = {
+        background = {
+          path = ../../assets/wallpapers/twilight-peaks.png;
+          fit = "Cover";
+        };
+        appearance = {
+          greeting_msg = "Welcome";
+        };
+        "widget.clock" = {
+          format = "%A %e %b %H:%M";
+          timezone = "Europe/Oslo";
+          label_width = 200;
+        };
+        GTK = {
+          application_prefer_dark_theme = true;
+        };
+      };
     };
   };
 }
