ada62ecf2dcc60a49aa367fd2c99adfd6f3c9c79
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-02 23:32:51 +0200

    feat: move back to regreet

--- hosts/desktop/programs.nix
+++ hosts/desktop/programs.nix
@@ -1,5 +1,40 @@
 { userName, pkgs, system, ... }: {
   programs = {
     dconf.enable = true;
+    regreet = {
+      enable = true;
+      cursorTheme = {
+        name = "macOS";
+        package = pkgs.apple-cursor;
+      };
+      font = {
+        name = "SF Pro";
+      };
+      theme = {
+        name = "Adwaita";
+        package = pkgs.gnome-themes-extra;
+      };
+      iconTheme = {
+        name = "Adwaita";
+        package = pkgs.adwaita-icon-theme;
+      };
+      settings = {
+        background = {
+          path = ../../assets/wallpapers/twilight-peaks.png;
+          fit = "Cover";
+        };
+        appearance = {
+          greeting_msg = "Welcome";
+        };
+        "widget.clock" = {
+          format = "%A %e %b %H:%M";
+          timezone = "Europe/Oslo";
+          label_width = 200;
+        };
+        GTK = {
+          application_prefer_dark_theme = true;
+        };
+      };
+    };
   };
 }
