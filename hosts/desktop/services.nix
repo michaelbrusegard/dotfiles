57ef7c0e900065bea7794d215a01749089b7bfb4
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-03 01:34:43 +0200

    feat: add hyprland to session options

--- hosts/desktop/services.nix
+++ hosts/desktop/services.nix
@@ -28,6 +28,7 @@
             misc {
               disable_hyprland_logo = true
               disable_splash_rendering = true
+              disable_hyprland_qtutils_check = true
             }
             exec-once = ${pkgs.greetd.regreet}/bin/regreet; ${pkgs.hyprland}/bin/hyprctl dispatch exit
           ''}";
