66bbea4e1049f7cf09cea6c3f0d17b9e5ecebcc9
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-02 21:44:41 +0200

    feat: swith to tuigreet

--- hosts/desktop/services.nix
+++ hosts/desktop/services.nix
@@ -15,6 +15,38 @@
         support32Bit = true;
       };
     };
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          user = "greeter";
+          command = ''
+            ${pkgs.greetd.tuigreet}/bin/tuigreet \
+              --time \
+              --time-format '%A %e %B %H:%M' \
+              --greeting "Authenticate into Desktop" \
+              --remember \
+              --remember-user-session \
+              --user-menu \
+              --user-menu-min-uid 1000 \
+              --user-menu-max-uid 60000 \
+              --asterisks \
+              --asterisks-char '●' \
+              --cmd Hyprland \
+              --width 100 \
+              --container-padding 2 \
+              --prompt-padding 1 \
+              --greet-align center \
+              --power-shutdown '${pkgs.systemd}/bin/loginctl poweroff' \
+              --power-reboot '${pkgs.systemd}/bin/loginctl reboot' \
+              --theme 'border=blue;text=white;prompt=cyan;input=yellow;action=green;button=magenta;container=black;time=gray' \
+              --kb-command 1 \
+              --kb-sessions 2 \
+              --kb-power 12
+          '';
+        };
+      };
+    };
     xserver.xkb.layout = "us";
   };
 }
