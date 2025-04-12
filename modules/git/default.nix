9fb7a576a7b124077ddc804d9f084f6fb05dab6f
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-12 14:51:54 +0200

    feat: switch to new sops github ssh key

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -14,8 +14,7 @@ in {
         userEmail = "56915010+michaelbrusegard@users.noreply.github.com";
         signing = {
           format = "ssh";
-          key = "~/.ssh/id_github";
-          # key = config.sops.secrets."hosts/github/sshKey".path;
+          key = config.sops.secrets."hosts/github/sshKey".path;
           signByDefault = true;
         };
         delta = {
