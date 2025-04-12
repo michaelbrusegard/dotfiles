927d3ea8439309e400c39772d18bb1a77bf2c441
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-12 13:39:58 +0200

    chore: remove session vars

--- hosts/desktop/environment.nix
+++ hosts/desktop/environment.nix
@@ -3,8 +3,4 @@
   environment.systemPackages = with pkgs; [
     qemu
   ];
-  environment.sessionVariables = {
-    LOCALE_ARCHIVE_2_11 = "${pkgs.glibcLocales}/lib/locale/locale-archive";
-    LOCALE_ARCHIVE_2_27 = "${pkgs.glibcLocales}/lib/locale/locale-archive";
-  };
 }
