b1a7c8a1c2f46a1ecf0615cdb783c46fe5d7e098
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-12 15:18:38 +0200

    fix: davinci resolve

--- hosts/desktop/hardware.nix
+++ hosts/desktop/hardware.nix
@@ -1,9 +1,14 @@
-{ modulesPath, ... }: {
+{ pkgs, modulesPath, ... }: {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
   hardware = {
     enableRedistributableFirmware = true;
     cpu.amd.updateMicrocode = true;
-    graphics.enable = true;
+    graphics = {
+      enable = true;
+      extraPackages = with pkgs; [
+        rocmPackages.clr.icd
+      ];
+    };
     bluetooth = {
       enable = true;
       powerOnBoot = true;
