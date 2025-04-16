b500a17d5c942d41d3387bc4b7b5c763ea399d38
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-16 16:59:32 +0200

    chore: fix presentation keybind conflict

--- modules/wezterm/config/lua/plugins/presentation.lua
+++ modules/wezterm/config/lua/plugins/presentation.lua
@@ -9,7 +9,7 @@ return function(config)
       disabled = true,
     },
     presentation_full = {
-      keybind = { key = 'p', mods = 'SHIFT|SUPER' },
+      keybind = { key = 'x', mods = 'SUPER' },
     },
   })
 end
