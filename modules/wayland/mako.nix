40f44aca481f2dddb73178eb3b11d6b62d47380f
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-16 06:30:24 +0200

    feat: update mako theme

--- modules/wayland/mako.nix
+++ modules/wayland/mako.nix
@@ -6,17 +6,22 @@ in {
   config = lib.mkIf cfg.enable {
     services.mako = {
       enable = true;
-      borderRadius = 8;
+      borderRadius = 3;
+      borderSize = 1;
       defaultTimeout = 5000;
-      width = 360;
-      height = 80;
+      width = 400;
+      height = 100;
       font = "SF Pro 11";
-      margin = "20";
+      margin = "16";
       padding = "15";
-      backgroundColor = "#1E1E1ECC";
-      textColor = "#FFFFFF";
-      borderColor = "#1E1E1E00";
+      backgroundColor = "#1e2021ee";
+      textColor = "#ffffff";
+      borderColor = "#a0c4e2";
+      progressColor = "over #a0c4e2";
       layer = "overlay";
+      icons = true;
+      maxIconSize = 48;
+      iconPath = "/usr/share/icons/Papirus-Dark";
     };
   };
 }
