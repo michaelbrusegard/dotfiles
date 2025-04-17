7550991992f9d4af8a459cedab0d671f27b51060
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-18 00:27:49 +0200

    feat: update secrets and try to fix monitor waking issue

--- modules/wayland/hyprland.nix
+++ modules/wayland/hyprland.nix
@@ -13,9 +13,10 @@ in {
       settings = {
         monitor = [
           "DP-1,3440x1440@144,0x0,1, bitdepth, 10"
-          "DP-3,2560x1440@144,auto,1, bitdepth, 10"
+          "DP-3,2560x1440@144,3440x0,1, bitdepth, 10"
         ];
         exec-once = [ 
+          "hyprctl dispatch dpms on; sleep 1; hyprctl dispatch focusmonitor DP-1"
           "wl-paste --type text --watch cliphist store"
           "wl-paste --type image --watch cliphist store"
         ];
