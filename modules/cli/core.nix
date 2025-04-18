b946c2046d1acc8ca551eb57305ff36b684f7ce6
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-18 16:24:22 +0200

    refactor: seperate cli packages into core and desktop

---
+++ modules/cli/core.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.modules.cli.core;
+in {
+  options.modules.cli.core.enable = lib.mkEnableOption "Core CLI applications";
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      coreutils
+      gnused
+      gnugrep
+      curl
+      wget
+      file
+      rsync
+      fontconfig
+      yq
+      age
+      sops
+    ];
+  };
+}
