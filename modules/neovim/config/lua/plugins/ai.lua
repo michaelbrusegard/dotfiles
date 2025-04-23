cb3a204528fd73e91e011fa5c19361d5ebd25d16
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-23 15:09:27 +0200

    chore: move blink config to coding.lua

--- modules/neovim/config/lua/plugins/ai.lua
+++ modules/neovim/config/lua/plugins/ai.lua
@@ -25,41 +25,6 @@ return {
     build = 'make',
   },
   {
-    'saghen/blink.cmp',
-    lazy = true,
-    dependencies = { 'saghen/blink.compat' },
-    opts = {
-      sources = {
-        default = { 'avante_commands', 'avante_mentions', 'avante_files' },
-        compat = {
-          'avante_commands',
-          'avante_mentions',
-          'avante_files',
-        },
-        providers = {
-          avante_commands = {
-            name = 'avante_commands',
-            module = 'blink.compat.source',
-            score_offset = 90,
-            opts = {},
-          },
-          avante_files = {
-            name = 'avante_files',
-            module = 'blink.compat.source',
-            score_offset = 100,
-            opts = {},
-          },
-          avante_mentions = {
-            name = 'avante_mentions',
-            module = 'blink.compat.source',
-            score_offset = 1000,
-            opts = {},
-          },
-        },
-      },
-    },
-  },
-  {
     'MeanderingProgrammer/render-markdown.nvim',
     ft = function(_, ft)
       vim.list_extend(ft, { 'Avante' })
