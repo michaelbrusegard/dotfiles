d084ce473f895b26f9f6714fdcf1a5ba1faf6612
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-27 13:58:55 +0200

    chore: improve hypridle

--- modules/wayland/hypridle.nix
+++ modules/wayland/hypridle.nix
@@ -16,13 +16,16 @@ in {
         listener = [
           {
             timeout = 300;
-            on-timeout = "loginctl lock-session && brightnessctl -sd rgb:kbd_backlight set 0";
-            on-resume = "brightnessctl -rd rgb:kbd_backlight";
+            on-timeout = "loginctl lock-session";
           }
           {
             timeout = 1200;
             on-timeout = "hyprctl dispatch dpms off";
             on-resume = "hyprctl dispatch dpms on";
+          }
+          {
+            timeout = 1800;
+            on-timeout = "systemctl suspend";
           }
         ];
       };
