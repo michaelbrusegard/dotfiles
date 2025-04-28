7219711b1519bcc95cc9c0a8978788012895fa24
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-29 00:09:20 +0200

    feat: add inkscape and gimp

--- hosts/darwin/homebrew.nix
+++ hosts/darwin/homebrew.nix
@@ -4,25 +4,21 @@
     caskArgs.no_quarantine = true;
     global.brewfile = true;
     casks = [
-      element
-      altserver
-      legcord
-      microsoft-outlook
-      obsidian
-      proton-mail
-      proton-pass
-      protonvpn
-      proton-drive
-      riscv-tools
+      "element-desktop"
+      "altserver"
+      "legcord"
+      "microsoft-outlook"
+      "obsidian"
+      "proton-mail-bridge"
+      "protonvpn"
+      "proton-drive"
+      "riscv-tools"
     ];
     masApps = {
       "Proton Pass for Safari" = 6502835663;
       "Wipr" = 1662217862;
       "WireGuard" = 1451685025;
       "Messenger" = 1480068668;
-      "Affinity Designer 2" = 1616831348;
-      "Affinity Photo 2" = 1616822987;
-      "Affinity Publisher 2" = 1606941598;
       "Keynote" = 409183694;
       "Numbers" = 409203825;
       "Pages" = 409201541;
