90d55e22379e80d2e642c56560dab8e99ce22ea0
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-30 22:11:22 +0200

    fix: kanata karabiner requirement driver

--- hosts/darwin/launchd.nix
+++ hosts/darwin/launchd.nix
@@ -1,29 +1,32 @@
 { pkgs, userName, ... }: {
-  launchd.user.agents = {
-    kanata = {
-      command = "${pkgs.kanata}/bin/kanata --cfg ${./kanata.kbd} --nodelay";
-      serviceConfig = {
-        UserName = userName;
-        RunAtLoad = true;
-        KeepAlive = {
-          SuccessfulExit = false;
-          Crashed = true;
+  launchd = {
+    daemons = {
+      kanata = {
+        command = "${pkgs.kanata}/bin/kanata --cfg ${./kanata.kbd} --nodelay";
+        serviceConfig = {
+          RunAtLoad = true;
+          KeepAlive = {
+            SuccessfulExit = false;
+            Crashed = true;
+          };
+          StandardErrorPath = "/Users/${userName}/.logs/kanata-error.log";
+          StandardOutPath = "/Users/${userName}/.logs/kanata-out.log";
+          ProcessType = "Interactive";
+          Nice = -30;
         };
-        StandardErrorPath = "/Users/${userName}/.logs/kanata-error.log";
-        StandardOutPath = "/Users/${userName}/.logs/kanata-out.log";
-        ProcessType = "Interactive";
-        Nice = -30;
       };
     };
-    podman = {
-      command = "${pkgs.podman}/bin/podman system service";
-      serviceConfig = {
-        UserName = userName;
-        RunAtLoad = true;
-        KeepAlive = true;
-        StandardErrorPath = "/Users/${userName}/.logs/podman-error.log";
-        StandardOutPath = "/Users/${userName}/.logs/podman-out.log";
-        ProcessType = "Background";
+    user.agents = {
+      podman = {
+        command = "${pkgs.podman}/bin/podman machine start";
+        serviceConfig = {
+          UserName = userName;
+          RunAtLoad = true;
+          KeepAlive = true;
+          StandardErrorPath = "/Users/${userName}/.logs/podman-error.log";
+          StandardOutPath = "/Users/${userName}/.logs/podman-out.log";
+          ProcessType = "Background";
+        };
       };
     };
   };
