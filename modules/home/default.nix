6f66d18314bf2f9d8c02098f069b773df6b50c3d
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-30 13:36:10 +0200

    chore: try darwin rebuild for package

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -94,7 +94,7 @@ in
     ];
     shellAliases = {
       rebuild = if isDarwin then
-        "sudo darwin-rebuild switch --flake $HOME/Developer/dotfiles#${hostName}"
+        "sudo ${pkgs.darwin-rebuild}/bin/darwin-rebuild switch --flake $HOME/Developer/dotfiles#${hostName}"
       else
         "sudo ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake $HOME/Developer/dotfiles#${hostName}";
       update = "${pkgs.nix}/bin/nix flake update --flake $HOME/Developer/dotfiles";
