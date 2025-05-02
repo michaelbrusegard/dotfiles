0459d0f91f6c8b91390b7e50cc0ac49d75fed941
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-05-02 17:16:06 +0200

    fix: podman launchd service and init machine on activation

--- hosts/darwin/launchd.nix
+++ hosts/darwin/launchd.nix
@@ -37,7 +37,6 @@
         serviceConfig = {
           UserName = userName;
           RunAtLoad = true;
-          KeepAlive = false;
           ProcessType = "Interactive";
         };
       };
@@ -46,7 +45,6 @@
         serviceConfig = {
           UserName = userName;
           RunAtLoad = true;
-          KeepAlive = false;
           ProcessType = "Interactive";
         };
       };
@@ -55,7 +53,6 @@
         serviceConfig = {
           UserName = userName;
           RunAtLoad = true;
-          KeepAlive = true;
           ProcessType = "Background";
         };
       };
