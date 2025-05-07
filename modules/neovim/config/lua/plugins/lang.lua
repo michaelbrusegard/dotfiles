2c7d1165795abab057333ff749cb5af1a95cdc9e
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-05-08 01:07:03 +0200

    feat: add latex support to neovim

--- modules/neovim/config/lua/plugins/lang.lua
+++ modules/neovim/config/lua/plugins/lang.lua
@@ -13,6 +13,7 @@ return {
   { import = 'lazyvim.plugins.extras.lang.python' },
   { import = 'lazyvim.plugins.extras.lang.rust' },
   { import = 'lazyvim.plugins.extras.lang.tailwind' },
+  { import = 'lazyvim.plugins.extras.lang.tex' },
   { import = 'lazyvim.plugins.extras.lang.toml' },
   { import = 'lazyvim.plugins.extras.lang.typescript' },
 }
