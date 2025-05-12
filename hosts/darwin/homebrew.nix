13b628de367d1e9b557ac4257afc257450ba35c7
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-05-12 23:46:01 +0200

    chore: add keynote pages and numbers to darwin

--- hosts/darwin/homebrew.nix
+++ hosts/darwin/homebrew.nix
@@ -33,9 +33,12 @@
       "Proton Pass for Safari" = 6502835663;
       "Wipr" = 1662217862;
       "WireGuard" = 1451685025;
-      "Messenger" = 1480068668;
+      "Keynote" = 409183694;
+      "Pages" = 409201541;
+      "Numbers" = 409203825;
       "Developer" = 640199958;
       "Xcode" = 497799835;
+      "Messenger" = 1480068668;
       "DaVinci Resolve" = 571213070;
     };
   };
