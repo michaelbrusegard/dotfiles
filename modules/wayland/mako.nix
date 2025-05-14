0b1adf68c568b9f820f36b1f729723d2e7a60c32
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-05-14 17:01:43 +0200

    chore: update mako

--- modules/wayland/mako.nix
+++ modules/wayland/mako.nix
@@ -6,18 +6,20 @@ in {
   config = lib.mkIf cfg.enable {
     services.mako = {
       enable = true;
-      borderRadius = 8;
-      borderSize = 1;
-      defaultTimeout = 5000;
-      width = 360;
-      height = 80;
-      font = "SFPro Nerd Font 12";
-      margin = "16";
-      padding = "16";
-      backgroundColor = "#1e1e1edd";
-      textColor = "#ffffff";
-      borderColor = "#7e7e7e";
-      layer = "overlay";
+      settings = {
+        border-radius = 8;
+        border-size = 1;
+        default-timeout = 5000;
+        width = 360;
+        height = 80;
+        font = "SFPro Nerd Font 12";
+        margin = "16";
+        padding = "16";
+        background-color = "#1e1e1edd";
+        text-color = "#ffffff";
+        border-color = "#7e7e7e";
+        layer = "overlay";
+      };
     };
   };
 }
