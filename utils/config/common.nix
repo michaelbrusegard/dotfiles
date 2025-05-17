4031b512ccd918e20bc6339c52c935276d735694
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-05-17 22:31:14 +0200

    feat: make main user trusted to be able to use trusted caches

--- utils/config/common.nix
+++ utils/config/common.nix
@@ -17,6 +17,7 @@
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
+      trusted-users = [ userName ];
     };
   };
   nixpkgs = {
