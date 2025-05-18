88870e8e66447857aa3d352d465982a606d82633
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-05-18 13:07:52 +0200

    Revert "feat: use raspberry pi cache instead of setting trusted user"
    This reverts commit 9fa84806556ed7aa30d2742ab353d8de83a0daa3.

--- utils/config/common.nix
+++ utils/config/common.nix
@@ -12,13 +12,12 @@
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
-        "https://nixos-raspberrypi.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
       ];
+      trusted-users = [ userName ];
     };
   };
   nixpkgs = {
