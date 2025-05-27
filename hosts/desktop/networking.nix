857a66a6df987ae320ba70888c94cccface5ef9d
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-05-28 01:23:57 +0200

    feat: change to iwd for networking

--- hosts/desktop/networking.nix
+++ hosts/desktop/networking.nix
@@ -3,9 +3,11 @@
     firewall = {
       allowedTCPPorts = [ 22 ];
     };
+    wireless.iwd.enable = true;
     networkmanager = {
       enable = true;
       dhcp = "dhcpcd";
+      wifi.backend = "iwd";
     };
     dhcpcd.enable = true;
     wireguard.enable = true;
