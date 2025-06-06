babce3c8671c6d17f0095c9f497af910f97dc8f8
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-06-06 14:39:17 +0200

    chore: add submodules to pico sdk to make usb work

--- shells/pico/default.nix
+++ shells/pico/default.nix
@@ -1,12 +1,18 @@
 { pkgs, ... }:
+let
+  pico-sdk-with-submodules = pkgs.pico-sdk.override {
+    withSubmodules = true;
+  };
+in
 pkgs.mkShell {
+  name = "pico-dev";
   buildInputs = with pkgs; [
     cmake
     gcc-arm-embedded
     picotool
-    pico-sdk
+    pico-sdk-with-submodules
   ];
-  PICO_SDK_PATH = "${pkgs.pico-sdk}";
+  PICO_SDK_PATH = "${pico-sdk-with-submodules}/lib/pico-sdk";
   CC = "${pkgs.gcc-arm-embedded}/bin/arm-none-eabi-gcc";
   CXX = "${pkgs.gcc-arm-embedded}/bin/arm-none-eabi-g++";
 }
