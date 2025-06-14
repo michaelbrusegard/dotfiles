d59d19d1f5b49a52bd5c02804af9b8808e3e59a2
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-06-14 19:45:31 +0200

    chore: remove wrong ps1 values

--- windows/programs/powershell/profile.ps1
+++ windows/programs/powershell/profile.ps1
@@ -7,12 +7,9 @@ Set-PSReadLineOption -HistoryNoDuplicates
 Set-PSReadLineOption -AddToHistoryHandler { param($line) -return $line -notmatch '^\s' }
 Set-PSReadLineOption -MaximumHistoryCount 10000
 Set-PSReadLineOption -ViModeIndicator Cursor
-Set-PSReadLineOption -ViCommandModeTimeout 10
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -ShowToolTips
 Set-PSReadLineKeyHandler -Key "Ctrl+p" -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key "Ctrl+n" -Function HistorySearchForward
 Set-PSReadLineKeyHandler -Key "Ctrl+y" -Function AcceptSuggestion
 Set-PSReadLineKeyHandler -Key "Ctrl+e" -Function ClearSuggestion
-Set-PSReadLineKeyHandler -Key "Ctrl+j" -Function MenuNext
-Set-PSReadLineKeyHandler -Key "Ctrl+k" -Function MenuPrevious
