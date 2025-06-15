19df489f9274c79181a777e21529dea6023554cc
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-06-15 03:30:50 +0200

    feat: add base services to leggero

--- hosts/leggero/services.nix
+++ hosts/leggero/services.nix
@@ -1,2 +1,33 @@
-{ ... }: {
+{ config, ... }: {
+  services = {
+    openssh = {
+      enable = true;
+      openFirewall = true;
+      startWhenNeeded = true;
+      ports = config.secrets.leggeroSshPorts;
+      authorizedKeysInHomedir = false;
+      authorizedKeysFiles = config.secrets.leggeroAuthorizedKeysFiles;
+      hostKeys = [];
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+        PermitRootLogin = "no";
+      };
+    };
+    fail2ban = {
+      enable = true;
+      bantime = "1h";
+    };
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      nssmdns6 = true;
+      allowInterfaces = [ "eth0" ];
+      publish = {
+        enable = true;
+        addresses = true;
+        workstation = true;
+      };
+    };
+  };
 }
