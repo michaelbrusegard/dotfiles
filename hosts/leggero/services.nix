225317032f53c668b94ceba15a5bf57203fd3fdd
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-06-22 16:16:01 +0200

    fix: use pkgs-otbr instead of nixpkgs-otbr for the module import

--- hosts/leggero/services.nix
+++ hosts/leggero/services.nix
@@ -1,6 +1,6 @@
-{ pkgs, config, userName, nixpkgs-otbr, ... }: {
+{ pkgs, config, userName, pkgs-otbr, ... }: {
   imports = [
-    "${nixpkgs-otbr}/nixos/modules/services/home-automation/openthread-border-router.nix"
+    "${pkgs-otbr.path}/nixos/modules/services/home-automation/openthread-border-router.nix"
   ];
   services = {
     openssh = {
