1ba170ad75ae29f2451714b687ea9a4d6195e7ab
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-06-22 17:06:35 +0200

    feat: seet openthread border router branch

--- hosts/leggero/boot.nix
+++
@@ -1,6 +0,0 @@
-{ ... }: {
-  boot.kernel = {
-    sysctl."net.ipv4.ip_forward" = true;
-    sysctl."net.ipv6.conf.all.forwarding" = true;
-  };
-}
--- hosts/leggero/default.nix
+++ hosts/leggero/default.nix
@@ -1,6 +1,5 @@
 { ... }: {
   imports = [
-    ./boot.nix
     ./hardware.nix
     ./networking.nix
     ./services.nix
