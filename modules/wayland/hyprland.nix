d97fd8279de59213d675e418167c6f65e63a2249
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-08-13 07:27:03 +0200

    chore: add specific spaces for freecad and notion

--- modules/wayland/hyprland.nix
+++ modules/wayland/hyprland.nix
@@ -151,12 +151,14 @@ in {
           "workspace 3, class:^(Proton Pass)$"
           "workspace 4, class:^(obsidian)$"
           "workspace 4, class:^(libreoffice)$"
+          "workspace 4, class:^(Notion)$"
           "workspace 5, class:^(legcord)$"
           "workspace 5, class:^(Element)$"
           "workspace 5, class:^(Slack)$"
           "workspace 7, class:^(resolve)$"
           "workspace 7, class:^(Gimp)$"
           "workspace 7, class:^(org.inkscape.Inkscape)$"
+          "workspace 7, class:^(FreeCAD)$"
         ];
       };
     };
