e73e73128abacb502ed641c1cbe52e34bcbf73b6
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-08-24 12:09:52 +0200

    feat: add sql config to neovim

--- modules/neovim/config/lua/plugins/lang.lua
+++ modules/neovim/config/lua/plugins/lang.lua
@@ -1,3 +1,4 @@
+local sql_ft = { "sql", "mysql", "plsql" }
 return {
   { import = 'lazyvim.plugins.extras.lang.angular' },
   { import = 'lazyvim.plugins.extras.lang.cmake' },
@@ -10,12 +11,13 @@ return {
   { import = 'lazyvim.plugins.extras.lang.markdown' },
   { import = 'lazyvim.plugins.extras.lang.nix' },
   { import = 'lazyvim.plugins.extras.lang.omnisharp' },
-  { import = 'lazyvim.plugins.extras.lang.python' },
   { import = 'lazyvim.plugins.extras.lang.rust' },
   { import = 'lazyvim.plugins.extras.lang.tailwind' },
   { import = 'lazyvim.plugins.extras.lang.tex' },
   { import = 'lazyvim.plugins.extras.lang.toml' },
   { import = 'lazyvim.plugins.extras.lang.typescript' },
+  -- Python
+  { import = 'lazyvim.plugins.extras.lang.python' },
   {
     'benomahony/uv.nvim',
     opts = {
@@ -24,5 +26,47 @@ return {
         prefix = '<leader>cu',
       },
     },
+  },
+  -- SQL
+  recommended = function()
+    return LazyVim.extras.wants({
+      ft = sql_ft,
+    })
+  end,
+  {
+    "kndndrj/nvim-dbee",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    build = function()
+      require("dbee").install()
+    end,
+    keys = {
+      { "<leader>D", function()
+        require("dbee").toggle()
+      end, desc = "Toggle DBee" },
+    },
+    opts = {};
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = { ensure_installed = { "sql" } },
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = { ensure_installed = { "sqlfluff" } },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters.sqlfluff = {
+        args = { "format", "--dialect=ansi", "-" },
+      }
+      for _, ft in ipairs(sql_ft) do
+        opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
+        table.insert(opts.formatters_by_ft[ft], "sqlfluff")
+      end
+    end,
   },
 }
