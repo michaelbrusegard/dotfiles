35eb4bc39ead59540932e5708fb8aa353afd1bd9
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-09-18 07:53:54 +0200

    feat: add django testing and debugging to neovim

--- modules/neovim/config/lua/plugins/dap.lua
+++ modules/neovim/config/lua/plugins/dap.lua
@@ -1,4 +1,24 @@
 return {
   { import = 'lazyvim.plugins.extras.dap.core' },
   { import = 'lazyvim.plugins.extras.dap.nlua' },
+  {
+    'mfussenegger/nvim-dap-python',
+    config = function()
+      local dap_python = require('dap-python')
+      dap_python.setup('python')
+      dap_python.test_runner = 'pytest'
+    end,
+  },
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap-python' },
+    config = function()
+      local dap = require('dap')
+      local dapui = require('dapui')
+      dapui.setup()
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open()
+      end
+    end,
+  },
 }
