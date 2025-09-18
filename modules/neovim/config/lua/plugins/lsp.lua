e3424f6deaf12e8611fce59c7d85bcd5a63ae9fb
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-09-18 08:31:42 +0200

    feat: disable strict type checking in python because most codebaes give too many diagnostics

--- modules/neovim/config/lua/plugins/lsp.lua
+++ modules/neovim/config/lua/plugins/lsp.lua
@@ -1,13 +1,38 @@
 return {
   {
     'neovim/nvim-lspconfig',
-    opts = function()
+    opts = function(_, opts)
       local keys = require('lazyvim.plugins.lsp.keymaps').get()
-      keys[#keys + 1] =
-        { 'crr', vim.lsp.buf.code_action, desc = 'Code Action', mode = { 'n', 'v' }, has = 'codeAction' }
-      keys[#keys + 1] = { 'crn', "<cmd>lua require('live-rename').rename()<cr>", desc = 'Rename', has = 'rename' }
-      keys[#keys + 1] =
-        { '<leader>cr', "<cmd>lua require('live-rename').rename()<cr>", desc = 'Rename', has = 'rename' }
+      keys[#keys + 1] = {
+        'crr',
+        vim.lsp.buf.code_action,
+        desc = 'Code Action',
+        mode = { 'n', 'v' },
+        has = 'codeAction',
+      }
+      keys[#keys + 1] = {
+        'crn',
+        "<cmd>lua require('live-rename').rename()<cr>",
+        desc = 'Rename',
+        has = 'rename',
+      }
+      keys[#keys + 1] = {
+        '<leader>cr',
+        "<cmd>lua require('live-rename').rename()<cr>",
+        desc = 'Rename',
+        has = 'rename',
+      }
+
+      opts.servers = opts.servers or {}
+      opts.servers.pyright = {
+        settings = {
+          python = {
+            analysis = {
+              typeCheckingMode = 'off',
+            },
+          },
+        },
+      }
     end,
   },
 }
