cd26ebe95ccfd33c01d96f9966b2e5ba762b8377
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-10-18 11:57:03 +0200

    chore: update nix and add opencode

--- users/michaelbrusegard/default.nix
+++ users/michaelbrusegard/default.nix
@@ -9,6 +9,7 @@
     gui.enable = !(isDarwin || isWsl);
     mpv.enable = !isWsl;
     neovim.enable = true;
+    opencode.enable = true;
     pentest.enable = true;
     shell.enable = true;
     silicon.enable = true;
