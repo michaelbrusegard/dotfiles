85ac778a527f8aacda268b18e84d922642ffb6ac
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-10-19 09:31:19 +0200

    chore: remove dprint

--- modules/neovim/config/lua/plugins/formatting.lua
+++ modules/neovim/config/lua/plugins/formatting.lua
@@ -1,20 +1,4 @@
 return {
   { import = 'lazyvim.plugins.extras.formatting.biome' },
   { import = 'lazyvim.plugins.extras.formatting.prettier' },
-  {
-    'stevearc/conform.nvim',
-    opts = {
-      formatters = {
-        dprint = {
-          condition = function(ctx)
-            return vim.fs.find({ 'dprint.json' }, { path = ctx.filename, upward = true })[1]
-          end,
-        },
-      },
-    },
-  },
-  {
-    'mason-org/mason.nvim',
-    opts = { ensure_installed = { 'dprint' } },
-  },
 }
