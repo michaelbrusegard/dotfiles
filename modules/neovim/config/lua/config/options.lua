f1cf74dc88613225cae3fbcdb9768f16f291281b
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-10-30 23:50:42 +0100

    feat: change python lsp and move to default lsp keybinds

--- modules/neovim/config/lua/config/options.lua
+++ modules/neovim/config/lua/config/options.lua
@@ -15,3 +15,6 @@ vim.g.lazyvim_prettier_needs_config = true
 
 -- Set zathura as the default latex viewer
 vim.g.vimtex_view_method = 'zathura'
+
+-- Use basedpyright
+vim.g.lazyvim_python_lsp = 'basedpyright'
--- modules/neovim/config/lua/plugins/lsp.lua
+++ modules/neovim/config/lua/plugins/lsp.lua
@@ -1,37 +1,77 @@
 return {
   {
     'neovim/nvim-lspconfig',
-    opts = function(_, opts)
+    opts = function(_, _)
       local keys = require('lazyvim.plugins.lsp.keymaps').get()
       keys[#keys + 1] = {
-        'crr',
+        'gra',
         vim.lsp.buf.code_action,
         desc = 'Code Action',
         mode = { 'n', 'v' },
         has = 'codeAction',
       }
       keys[#keys + 1] = {
-        'crn',
+        'gri',
+        vim.lsp.buf.implementation,
+        desc = 'Go to Implementation',
+        mode = 'n',
+        has = 'implementation',
+      }
+      keys[#keys + 1] = {
+        'grn',
         '<cmd>lua require("live-rename").rename()<cr>',
         desc = 'Rename',
+        mode = 'n',
         has = 'rename',
       }
       keys[#keys + 1] = {
         '<leader>cr',
         '<cmd>lua require("live-rename").rename()<cr>',
         desc = 'Rename',
+        mode = 'n',
         has = 'rename',
       }
-
-      opts.servers = opts.servers or {}
-      opts.servers.pyright = {
-        settings = {
-          python = {
-            analysis = {
-              typeCheckingMode = 'off',
-            },
-          },
-        },
+      keys[#keys + 1] = {
+        'grr',
+        vim.lsp.buf.references,
+        desc = 'Go to References',
+        mode = 'n',
+        has = 'references',
+      }
+      keys[#keys + 1] = {
+        'grt',
+        vim.lsp.buf.type_definition,
+        desc = 'Go to Type Definition',
+        mode = 'n',
+        has = 'typeDefinition',
+      }
+      keys[#keys + 1] = {
+        'gO',
+        vim.lsp.buf.document_symbol,
+        desc = 'Document Symbols',
+        mode = 'n',
+        has = 'documentSymbol',
+      }
+      keys[#keys + 1] = {
+        '<C-s>',
+        vim.lsp.buf.signature_help,
+        desc = 'Signature Help',
+        mode = 'i',
+        has = 'signatureHelp',
+      }
+      keys[#keys + 1] = {
+        'an',
+        vim.lsp.buf.selection_range,
+        desc = 'Outer Selection Range',
+        mode = 'v',
+        has = 'selectionRange',
+      }
+      keys[#keys + 1] = {
+        'in',
+        vim.lsp.buf.selection_range,
+        desc = 'Inner Selection Range',
+        mode = 'v',
+        has = 'selectionRange',
       }
     end,
   },
