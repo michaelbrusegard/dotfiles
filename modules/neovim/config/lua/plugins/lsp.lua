81f46e5ca02cb4d59beef3984413db93a3ddaca9
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-10-31 16:09:26 +0100

    fix: nvim lspconfig

--- modules/neovim/config/lua/plugins/lsp.lua
+++ modules/neovim/config/lua/plugins/lsp.lua
@@ -1,78 +1,83 @@
-return {
-  {
-    'neovim/nvim-lspconfig',
-    opts = function(_, _)
-      local keys = require('lazyvim.plugins.lsp.keymaps').get()
-      keys[#keys + 1] = {
-        'gra',
-        vim.lsp.buf.code_action,
-        desc = 'Code Action',
-        mode = { 'n', 'v' },
-        has = 'codeAction',
-      }
-      keys[#keys + 1] = {
-        'gri',
-        vim.lsp.buf.implementation,
-        desc = 'Go to Implementation',
-        mode = 'n',
-        has = 'implementation',
-      }
-      keys[#keys + 1] = {
-        'grn',
-        '<cmd>lua require("live-rename").rename()<cr>',
-        desc = 'Rename',
-        mode = 'n',
-        has = 'rename',
-      }
-      keys[#keys + 1] = {
-        '<leader>cr',
-        '<cmd>lua require("live-rename").rename()<cr>',
-        desc = 'Rename',
-        mode = 'n',
-        has = 'rename',
-      }
-      keys[#keys + 1] = {
-        'grr',
-        vim.lsp.buf.references,
-        desc = 'Go to References',
-        mode = 'n',
-        has = 'references',
-      }
-      keys[#keys + 1] = {
-        'grt',
-        vim.lsp.buf.type_definition,
-        desc = 'Go to Type Definition',
-        mode = 'n',
-        has = 'typeDefinition',
-      }
-      keys[#keys + 1] = {
-        'gO',
-        vim.lsp.buf.document_symbol,
-        desc = 'Document Symbols',
-        mode = 'n',
-        has = 'documentSymbol',
-      }
-      keys[#keys + 1] = {
-        '<C-s>',
-        vim.lsp.buf.signature_help,
-        desc = 'Signature Help',
-        mode = 'i',
-        has = 'signatureHelp',
-      }
-      keys[#keys + 1] = {
-        'an',
-        vim.lsp.buf.selection_range,
-        desc = 'Outer Selection Range',
-        mode = 'v',
-        has = 'selectionRange',
-      }
-      keys[#keys + 1] = {
-        'in',
-        vim.lsp.buf.selection_range,
-        desc = 'Inner Selection Range',
-        mode = 'v',
-        has = 'selectionRange',
-      }
-    end,
-  },
-}
+ return {
+   {
+     'neovim/nvim-lspconfig',
+     opts = {
+       servers = {
+         ['*'] = {
+           keys = {
+             {
+               'gra',
+               '<cmd>lua vim.lsp.buf.code_action()<CR>',
+               desc = 'Code Action',
+               mode = { 'n', 'v' },
+               has = 'codeAction',
+             },
+             {
+               'gri',
+               '<cmd>lua vim.lsp.buf.implementation()<CR>',
+               desc = 'Go to Implementation',
+               mode = 'n',
+               has = 'implementation',
+             },
+             {
+               'grn',
+               '<cmd>lua require("live-rename").rename()<cr>',
+               desc = 'Rename',
+               mode = 'n',
+               has = 'rename',
+             },
+             {
+               '<leader>cr',
+               '<cmd>lua require("live-rename").rename()<cr>',
+               desc = 'Rename',
+               mode = 'n',
+               has = 'rename',
+             },
+             {
+               'grr',
+               '<cmd>lua vim.lsp.buf.references()<CR>',
+               desc = 'Go to References',
+               mode = 'n',
+               has = 'references',
+             },
+             {
+               'grt',
+               '<cmd>lua vim.lsp.buf.type_definition()<CR>',
+               desc = 'Go to Type Definition',
+               mode = 'n',
+               has = 'typeDefinition',
+             },
+             {
+               'gO',
+               '<cmd>lua vim.lsp.buf.document_symbol()<CR>',
+               desc = 'Document Symbols',
+               mode = 'n',
+               has = 'documentSymbol',
+             },
+             {
+               '<C-s>',
+               '<cmd>lua vim.lsp.buf.signature_help()<CR>',
+               desc = 'Signature Help',
+               mode = 'i',
+               has = 'signatureHelp',
+             },
+             {
+               'an',
+               '<cmd>lua vim.lsp.buf.selection_range()<CR>',
+               desc = 'Outer Selection Range',
+               mode = 'v',
+               has = 'selectionRange',
+             },
+             {
+               'in',
+               '<cmd>lua vim.lsp.buf.selection_range()<CR>',
+               desc = 'Inner Selection Range',
+               mode = 'v',
+               has = 'selectionRange',
+             },
+           };
+         };
+       };
+     };
+   },
+ }
