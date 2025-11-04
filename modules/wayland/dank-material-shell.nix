83e0ad799cc64017a24050c940ae8ccd3e4b6d75
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-11-04 01:13:46 +0100

    fix: dank dupliccate issues

--- modules/wayland/dank-material-shell.nix
+++ modules/wayland/dank-material-shell.nix
@@ -11,9 +11,7 @@ in {
         enableSystemMonitoring = true;
         enableClipboard = true;
         enableVPN = true;
-        enableBrightnessControl = true;
         enableColorPicker = true;
-        enableDynamicTheming = true;
         enableAudioWavelength = true;
         enableCalendarEvents = true;
         enableSystemSound = true;
