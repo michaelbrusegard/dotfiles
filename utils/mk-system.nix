911da744fc01be6bc3e3252d46770b56f6eec7b0
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-11-06 23:50:03 +0100

    feat: update opencode

--- utils/mk-system.nix
+++ utils/mk-system.nix
@@ -5,6 +5,11 @@ let
   isAarch64Linux = system == "aarch64-linux";
   isWsl = hostName == "wsl";
 
+  pkgs-unstable-updated = import inputs.nixpkgs-unstable-updated {
+    inherit system;
+    config.allowUnfree = true;
+  };
+
   pkgs-unstable = import inputs.nixpkgs-unstable {
     inherit system;
     config.allowUnfree = true;
@@ -23,7 +28,7 @@ let
   commonArgs = {
     inherit system;
     specialArgs = {
-      inherit system userName hostName stateVersion isDarwin isWsl pkgs-unstable pkgs-otbr;
+      inherit system userName hostName stateVersion isDarwin isWsl pkgs-unstable pkgs-unstable-updated pkgs-otbr;
       inherit (inputs) nixpkgs nix-darwin home-manager sops-nix nixos-raspberrypi nixos-wsl nur lanzaboote mac-app-util nix-homebrew homebrew-core homebrew-cask homebrew-extras apple-fonts apple-emoji-linux catppuccin catppuccin-themes hyprland dgop dms-cli dankMaterialShell yazi wezterm fancontrol-gui fenix affinity dotfiles-private;
     };
   };
