0b3c92db5ac284d491d141bbf1b3b2b11f6e6bd9
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-12-04 12:29:06 +0100

    chore: cleanup django test stuffs

--- modules/neovim/config/lua/plugins/test.lua
+++ modules/neovim/config/lua/plugins/test.lua
@@ -1,23 +1,3 @@
 return {
   { import = 'lazyvim.plugins.extras.test.core' },
-  {
-    'nvim-neotest/neotest',
-    dependencies = { 'nvim-neotest/neotest-python' },
-    opts = function(_, opts)
-      local django = require('util.django')
-      local neotest_python = require('neotest-python')
-
-      opts.adapters = opts.adapters or {}
-      table.insert(
-        opts.adapters,
-        neotest_python({
-          runner = 'pytest',
-          env = {
-            DJANGO_SETTINGS_MODULE = django.get_django_settings(),
-          },
-          setup_command = { 'bash', '-c', 'DJANGO_SETTINGS_MODULE=' .. django.get_django_settings() .. ' pytest "$@"' },
-        })
-      )
-    end,
-  },
 }
