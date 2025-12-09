6676c8176827b8c7de00336976eb4e4fee2358e2
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-12-09 01:23:04 +0100

    chore: disable smear cursor for now

--- modules/neovim/config/lua/plugins/ui.lua
+++ modules/neovim/config/lua/plugins/ui.lua
@@ -1,6 +1,6 @@
 return {
   { 'akinsho/bufferline.nvim', enabled = false },
-  { import = 'lazyvim.plugins.extras.ui.smear-cursor' },
+  -- { import = 'lazyvim.plugins.extras.ui.smear-cursor' },
   {
     'nvim-lualine/lualine.nvim',
     opts = {
