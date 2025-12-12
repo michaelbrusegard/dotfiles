9c5dedd78808e34dfdcba92c3c59ff33eb6e5581
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-12-12 05:33:18 +0100

    feat: add yazi and wezterm cachix

--- utils/config/common.nix
+++ utils/config/common.nix
@@ -14,12 +14,16 @@
         "https://nix-community.cachix.org"
         "https://nixos-raspberrypi.cachix.org"
         "https://hyprland.cachix.org"
+        "https://wezterm.cachix.org"
+        "https://yazi.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
+        "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
       ];
     };
   };
