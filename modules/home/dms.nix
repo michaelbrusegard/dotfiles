a1ec62467756c4685c9c6b28b44a354afcd1ef76
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-01-02 19:49:54 +0100

    feat: update secrets with correct attributes

--- modules/home/dms.nix
+++ modules/home/dms.nix
@@ -8,26 +8,28 @@
 }: let
   dmsConfig = inputs.self + "/config/dms";
 in {
-  home.packages = lib.mkIf (pkgs.stdenv.isLinux && !isWsl) (with pkgs; [
-    polkit_gnome
-    grim
-    slurp
-    wf-recorder
-  ]);
+  home = {
+    packages = lib.mkIf (pkgs.stdenv.isLinux && !isWsl) (with pkgs; [
+      polkit_gnome
+      grim
+      slurp
+      wf-recorder
+    ]);
 
-  home.sessionVariables = lib.mkIf (pkgs.stdenv.isLinux && !isWsl) {
-    QT_QPA_PLATFORM = "wayland";
-    ELECTRON_OZONE_PLATFORM_HINT = "auto";
-    QT_QPA_PLATFORMTHEME = "gtk3";
-    QT_QPA_PLATFORMTHEME_QT6 = "gtk3";
-  };
+    sessionVariables = lib.mkIf (pkgs.stdenv.isLinux && !isWsl) {
+      QT_QPA_PLATFORM = "wayland";
+      ELECTRON_OZONE_PLATFORM_HINT = "auto";
+      QT_QPA_PLATFORMTHEME = "gtk3";
+      QT_QPA_PLATFORMTHEME_QT6 = "gtk3";
+    };
 
-  home.pointerCursor = lib.mkIf (pkgs.stdenv.isLinux && !isWsl) {
-    package = pkgs.apple-cursor;
-    name = "macOS";
-    size = 24;
-    gtk.enable = true;
-    x11.enable = true;
+    pointerCursor = lib.mkIf (pkgs.stdenv.isLinux && !isWsl) {
+      package = pkgs.apple-cursor;
+      name = "macOS";
+      size = 24;
+      gtk.enable = true;
+      x11.enable = true;
+    };
   };
 
   xdg.configFile = lib.mkIf (pkgs.stdenv.isLinux && !isWsl) {
