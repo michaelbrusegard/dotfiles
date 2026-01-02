1397c0b1e9adeb8bb8ce9872bc96e784b464db20
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-01-02 21:19:18 +0100

    fix: ops user home modules

--- users/ops/home.nix
+++ users/ops/home.nix
@@ -1,10 +1,10 @@
 {inputs, ...}: {
   imports = [
+    inputs.self.homeManagerModules.catppuccin
     inputs.self.homeManagerModules.cli-base
     inputs.self.homeManagerModules.git
     inputs.self.homeManagerModules.neovim
     inputs.self.homeManagerModules.shell
-    inputs.self.homeManagerModules.ssh
     inputs.self.homeManagerModules.wezterm
     inputs.self.homeManagerModules.yazi
   ];
