64401a0695f1892faada9c443cd93e87ebf87d50
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-01-02 20:50:07 +0100

    feat: export modules automatically

--- lib/default.nix
+++ lib/default.nix
@@ -27,11 +27,30 @@ inputs: let
         })
       hostnames
     );
+
+  exportModules = dir: let
+    files = builtins.readDir dir;
+    validFiles =
+      nixpkgs.lib.filterAttrs
+      (name: type:
+        type
+        == "regular"
+        && nixpkgs.lib.hasSuffix ".nix" name)
+      files;
+    nameFromPath = name: nixpkgs.lib.removeSuffix ".nix" name;
+  in
+    nixpkgs.lib.mapAttrs'
+    (name: _: {
+      name = nameFromPath name;
+      value = import (dir + "/${name}");
+    })
+    validFiles;
 in {
   inherit
     forAllSystems
     merge
     mkSystem
     mkCluster
+    exportModules
     ;
 }
