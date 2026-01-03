33217c1b98dccdd3a8038976403a37d4c2c71c3b
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-01-03 03:48:54 +0100

    feat: add disko and hardware config to espresso

--- hosts/espresso/default.nix
+++ hosts/espresso/default.nix
@@ -4,7 +4,9 @@
     inputs.self.nixosModules.catppuccin
     inputs.self.nixosModules.console
     inputs.self.nixosModules.disable-documentation
+    inputs.self.nixosModules.disko
     inputs.self.nixosModules.home-manager
+    inputs.self.nixosModules.lanzaboote
     inputs.self.nixosModules.locale
     inputs.self.nixosModules.networking
     inputs.self.nixosModules.nix
@@ -12,6 +14,7 @@
     ./k3s.nix
     ./networking.nix
     ./hardware.nix
+    ./disko.nix
   ];
 
   system.stateVersion = "25.11";
