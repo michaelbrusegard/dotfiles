88722b20bb2778b8c04268ab41fd0cdc3ca89060
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-01-03 02:48:42 +0100

    fix: nix formatter in neovim

--- config/neovim/lua/plugins/lang.lua
+++ config/neovim/lua/plugins/lang.lua
@@ -72,4 +72,17 @@ return {
       end
     end,
   },
+  -- Nix
+  {
+    'mason-org/mason.nvim',
+    opts = { ensure_installed = { 'alejandra' } },
+  },
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = {
+        nix = { 'alejandra' },
+      },
+    },
+  },
 }
