b90387e272393b686289ba76deb3865cd3926f85
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-01-04 20:19:42 +0100

    feat: add k3s cluster token and init for node 0

--- hosts/espresso/k3s.nix
+++ hosts/espresso/k3s.nix
@@ -1,8 +1,9 @@
-_: {
+{config}: {
   services.k3s = {
     enable = true;
-    clusterInit = true;
+    clusterInit = config.networking.hostName == "espresso-0";
     gracefulNodeShutdown.enable = true;
+    inherit (config.services.k3s) tokenFile;
     extraFlags = [
       "--write-kubeconfig-mode=0644"
     ];
