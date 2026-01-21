c9837ac7f7bb069d178c3002166dc99da93b3614
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-01-21 08:02:20 +0100

    feat: setup basic router config for macchiato

--- hosts/macchiato/networking.nix
+++ hosts/macchiato/networking.nix
@@ -1,14 +1,36 @@
-{config, ...}: {
+{config, ...}: let
+  wanInterface = "enp1s0";
+  lanInterfaces = ["enp2s0" "enp3s0" "sfp0"];
+in {
   networking = {
-    firewall = {
-      allowedTCPPorts = [53 9090 3000 1883 8080 8581];
-      allowedUDPPorts = [51820 53];
+    bridges.br0.interfaces = lanInterfaces;
+
+    interfaces = {
+      "${wanInterface}".useDHCP = true;
+      br0.ipv4.addresses = [
+        {
+          address = "10.0.186.1";
+          prefixLength = 24;
+        }
+      ];
     };
 
     nat = {
       enable = true;
-      internalInterfaces = ["wg0"];
-      externalInterface = "end0";
+      externalInterface = wanInterface;
+      internalInterfaces = ["br0" "wg0"];
+    };
+
+    firewall = {
+      enable = true;
+
+      interfaces.br0.allowedTCPPorts = [53 9090 3000 1883 8080 8581];
+      interfaces.br0.allowedUDPPorts = [53 67];
+
+      interfaces.wg0.allowedTCPPorts = [53 9090 3000 1883 8080 8581];
+      interfaces.wg0.allowedUDPPorts = [53];
+
+      interfaces."${wanInterface}".allowedUDPPorts = [51820];
     };
 
     wireguard.interfaces.wg0 = {
@@ -19,5 +41,18 @@
     };
   };
 
-  services.avahi.allowInterfaces = ["end0" "wg0"];
+  services.dnsmasq = {
+    enable = true;
+    settings = {
+      interface = "br0";
+      port = 0;
+      dhcp-range = "10.0.186.50,10.0.186.254,24h";
+      dhcp-option = [
+        "3,10.0.186.1"
+        "6,10.0.186.1"
+      ];
+    };
+  };
+
+  services.avahi.allowInterfaces = ["br0" "wg0"];
 }
