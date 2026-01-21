3632f30c1d24098a1b98760b4553f8e91d038280
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-01-21 08:40:36 +0100

    feat: add looad balancer for espresso to macchiato

--- hosts/macchiato/networking.nix
+++ hosts/macchiato/networking.nix
@@ -26,16 +26,19 @@ in {
 
       interfaces = {
         br0 = {
-          allowedTCPPorts = [53 9090 3000 1883 8080 8581];
+          allowedTCPPorts = [53 9090 3000 1883 8080 8581 6443];
           allowedUDPPorts = [53 67];
         };
 
         wg0 = {
-          allowedTCPPorts = [53 9090 3000 1883 8080 8581];
+          allowedTCPPorts = [53 9090 3000 1883 8080 8581 6443];
           allowedUDPPorts = [53];
         };
 
-        "${wanInterface}".allowedUDPPorts = [51820];
+        "${wanInterface}" = {
+          allowedTCPPorts = [80 443];
+          allowedUDPPorts = [51820];
+        };
       };
     };
 
@@ -47,18 +50,66 @@ in {
     };
   };
 
-  services.dnsmasq = {
-    enable = true;
-    settings = {
-      interface = "br0";
-      port = 0;
-      dhcp-range = "10.0.186.50,10.0.186.254,24h";
-      dhcp-option = [
-        "3,10.0.186.1"
-        "6,10.0.186.1"
-      ];
+  services = {
+    avahi.allowInterfaces = ["br0" "wg0"];
+
+    dnsmasq = {
+      enable = true;
+      settings = {
+        interface = "br0";
+        port = 0;
+        dhcp-range = "10.0.186.50,10.0.186.254,24h";
+        dhcp-option = [
+          "3,10.0.186.1"
+          "6,10.0.186.1"
+        ];
+        dhcp-host = [
+          "00:00:00:00:00:00,10.0.186.18,espresso-0"
+          "00:00:00:00:00:00,10.0.186.19,espresso-1"
+          "00:00:00:00:00:00,10.0.186.20,espresso-2"
+        ];
+      };
+    };
+
+    haproxy = {
+      enable = true;
+      config = ''
+        global
+          maxconn 4096
+
+        defaults
+          mode tcp
+          timeout client 30s
+          timeout server 30s
+          timeout connect 5s
+
+        frontend k3s_api
+          bind *:6443
+          default_backend k3s_servers
+        backend k3s_servers
+          balance roundrobin
+          server espresso-0 10.0.186.18:6443 check
+          server espresso-1 10.0.186.19:6443 check
+          server espresso-2 10.0.186.20:6443 check
+
+        frontend http_ing
+          bind *:80
+          default_backend http_servers
+        backend http_servers
+          balance roundrobin
+          server espresso-0 10.0.186.18:80 check
+          server espresso-1 10.0.186.19:80 check
+          server espresso-2 10.0.186.20:80 check
+
+        frontend https_ing
+          bind *:443
+          default_backend https_servers
+        backend https_servers
+          balance roundrobin
+          server espresso-0 10.0.186.18:443 check
+          server espresso-1 10.0.186.19:443 check
+          server espresso-2 10.0.186.20:443 check
+      '';
     };
   };
-
-  services.avahi.allowInterfaces = ["br0" "wg0"];
 }
