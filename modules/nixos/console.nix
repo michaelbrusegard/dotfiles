17ba014874fe354ed22998ca43ec884f2ec78446
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-01-22 17:42:30 +0100

    chore: add colors to console :)

--- modules/nixos/console.nix
+++ modules/nixos/console.nix
@@ -6,5 +6,23 @@
   console = lib.mkIf (!isWsl) {
     enable = true;
     keyMap = "us";
+    colors = [
+      "11111b"
+      "f38ba8"
+      "a6e3a1"
+      "f9e2af"
+      "89b4fa"
+      "cba6f7"
+      "94e2d5"
+      "a6adc8"
+      "313244"
+      "fab387"
+      "74c7ec"
+      "f5e0dc"
+      "b4befe"
+      "f5c2e7"
+      "89dceb"
+      "cdd6f4"
+    ];
   };
 }
