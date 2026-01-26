090534a810f590794fac171ed40a29e5a63d9f3f
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-01-27 00:35:36 +0100

    fix: install openisci cli tool and update longhorn

--- modules/nixos/openiscsi.nix
+++ modules/nixos/openiscsi.nix
@@ -1,7 +1,14 @@
-{config, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   services.openiscsi = {
     enable = true;
     enableAutoLoginOut = true;
     name = "iqn.2026-01.local.k3s:${config.networking.hostName}";
   };
+  environment.systemPackages = with pkgs; [
+    openiscsi
+  ];
 }
