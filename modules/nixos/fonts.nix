b911ddf58e90168e807894f294fce0f3a19acaf2
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-01-30 02:41:56 +0100

    fix: fontconfig

--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -1,16 +1,18 @@
 {pkgs, ...}: {
-  fonts.packages = with pkgs; [
-    roboto
-    roboto-serif
-    nerd-fonts.roboto-mono
-    noto-fonts-color-emoji
-    corefonts
-    inter
-  ];
-  fontconfig.defaultFonts = {
-    sansSerif = ["Roboto"];
-    serif = ["Roboto Serif"];
-    monospace = ["RobotoMono Nerd Font"];
-    emoji = ["Noto Color Emoji"];
+  fonts = {
+    packages = with pkgs; [
+      roboto
+      roboto-serif
+      nerd-fonts.roboto-mono
+      noto-fonts-color-emoji
+      corefonts
+      inter
+    ];
+    fontconfig.defaultFonts = {
+      sansSerif = ["Roboto"];
+      serif = ["Roboto Serif"];
+      monospace = ["RobotoMono Nerd Font"];
+      emoji = ["Noto Color Emoji"];
+    };
   };
 }
