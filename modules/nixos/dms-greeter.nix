9c4f39c887b880b4e374c018e406102219d544ab
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-02-01 14:10:29 +0100

    chore: move the display manager generic shim

--- modules/nixos/dms-greeter.nix
+++ modules/nixos/dms-greeter.nix
@@ -1,7 +1,6 @@
 {
   pkgs,
   inputs,
-  lib,
   users,
   ...
 }: {
@@ -9,35 +8,6 @@
   imports = [
     "${inputs.nixpkgs-unstable}/nixos/modules/services/display-managers/dms-greeter.nix"
   ];
-
-  # Provide compatibility shim for services.displayManager.generic
-  # which exists in nixpkgs-unstable but not in 25.11
-  options.services.displayManager.generic = {
-    enable =
-      lib.mkEnableOption "generic display manager integration"
-      // {
-        default = false;
-      };
-
-    preStart = lib.mkOption {
-      type = lib.types.lines;
-      default = "";
-      description = "Script executed before the display manager is started.";
-    };
-
-    execCmd = lib.mkOption {
-      type = lib.types.nullOr lib.types.str;
-      default = null;
-      description = "Command to start the display manager.";
-    };
-
-    environment = lib.mkOption {
-      type = with lib.types; attrsOf unspecified;
-      default = {};
-      description = "Additional environment variables needed by the display manager.";
-    };
-  };
-
   config = {
     environment.systemPackages = [pkgs.bibata-cursors];
 
--- modules/nixos/gtk.nix
+++ modules/nixos/gtk.nix
@@ -1,4 +1,34 @@
-_: {
+{lib, ...}: {
   gtk.iconCache.enable = true;
   programs.dconf.enable = true;
+
+  # TODO: Remove when updating to nixpkgs 26.05
+  # Provide compatibility shim for services.displayManager.generic
+  # which exists in nixpkgs-unstable but not in 25.11
+  options.services.displayManager.generic = {
+    enable =
+      lib.mkEnableOption "generic display manager integration"
+      // {
+        default = false;
+      };
+
+    preStart = lib.mkOption {
+      type = lib.types.lines;
+      default = "";
+      description = "Script executed before the display manager is started.";
+    };
+
+    execCmd = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = "Command to start the display manager.";
+    };
+
+    environment = lib.mkOption {
+      type = with lib.types; attrsOf unspecified;
+      default = {};
+      description = "Additional environment variables needed by the display manager.";
+    };
+  };
+
 }
