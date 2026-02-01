5c8fc54478a7e3f9310bd146f752e7ebd5c04bc0
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-02-01 13:30:51 +0100

    feat: add compatibility shim for dms-greeter

--- modules/nixos/dms-greeter.nix
+++ modules/nixos/dms-greeter.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   inputs,
+  lib,
   ...
 }: {
   # TODO: Remove when updating to nixpkgs 26.05
@@ -8,27 +9,57 @@
     "${inputs.nixpkgs-unstable}/nixos/modules/services/display-managers/dms-greeter.nix"
   ];
 
-  environment.systemPackages = [pkgs.bibata-cursors];
+  # Provide compatibility shim for services.displayManager.generic
+  # which exists in nixpkgs-unstable but not in 25.11
+  options.services.displayManager.generic = {
+    enable =
+      lib.mkEnableOption "generic display manager integration"
+      // {
+        default = false;
+      };
 
-  services.displayManager.dms-greeter = {
-    enable = true;
-    compositor.name = "hyprland";
-    compositor.customConfig = ''
-      env = XCURSOR_THEME,Bibata-Modern-Classic
-      env = XCURSOR_SIZE,24
-      input {
-        kb_layout = us
-        kb_variant = mac
-        kb_options = lv3:lalt_switch
-        repeat_rate = 65
-        repeat_delay = 150
-        follow_mouse = 1
-      }
-      misc {
-        disable_hyprland_logo = true
-        disable_splash_rendering = true
-        disable_watchdog_warning = true
-      }
-    '';
+    preStart = lib.mkOption {
+      type = lib.types.lines;
+      default = "";
+      description = "Script executed before the display manager is started.";
+    };
+
+    execCmd = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = "Command to start the display manager.";
+    };
+
+    environment = lib.mkOption {
+      type = with lib.types; attrsOf unspecified;
+      default = {};
+      description = "Additional environment variables needed by the display manager.";
+    };
+  };
+
+  config = {
+    environment.systemPackages = [pkgs.bibata-cursors];
+
+    services.displayManager.dms-greeter = {
+      enable = true;
+      compositor.name = "hyprland";
+      compositor.customConfig = ''
+        env = XCURSOR_THEME,Bibata-Modern-Classic
+        env = XCURSOR_SIZE,24
+        input {
+          kb_layout = us
+          kb_variant = mac
+          kb_options = lv3:lalt_switch
+          repeat_rate = 65
+          repeat_delay = 150
+          follow_mouse = 1
+        }
+        misc {
+          disable_hyprland_logo = true
+          disable_splash_rendering = true
+          disable_watchdog_warning = true
+        }
+      '';
+    };
   };
 }
