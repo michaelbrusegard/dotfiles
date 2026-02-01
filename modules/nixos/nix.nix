57c57bfde547b9e94325ed5786065dd607f8e0f1
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-02-01 14:25:37 +0100

    chore: move comppatibility shim to global nix

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,15 +1,46 @@
-_: {
+{lib, ...}: {
   imports = [
     ../common/nix.nix
   ];
-  nix = {
-    gc.dates = "weekly";
-    daemonCPUSchedPolicy = "idle";
-    daemonIOSchedClass = "idle";
-    settings = {
-      allowed-users = ["@wheel"];
-      trusted-users = ["@wheel"];
+  config = {
+    nix = {
+      gc.dates = "weekly";
+      daemonCPUSchedPolicy = "idle";
+      daemonIOSchedClass = "idle";
+      settings = {
+        allowed-users = ["@wheel"];
+        trusted-users = ["@wheel"];
+      };
+    };
+    users.mutableUsers = false;
+  };
+
+  # TODO: Remove when updating to nixpkgs 26.05
+  # Provide compatibility shim for services.displayManager.generic
+  # which exists in nixpkgs-unstable but not in 25.11
+  options.services.displayManager.generic = {
+    enable =
+      lib.mkEnableOption "generic display manager integration"
+      // {
+        default = false;
+      };
+
+    preStart = lib.mkOption {
+      type = lib.types.lines;
+      default = "";
+      description = "Script executed before the display manager is started.";
+    };
+
+    execCmd = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = "Command to start the display manager.";
+    };
+
+    environment = lib.mkOption {
+      type = with lib.types; attrsOf unspecified;
+      default = {};
+      description = "Additional environment variables needed by the display manager.";
     };
   };
-  users.mutableUsers = false;
 }
